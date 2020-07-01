6f6f2788b030a5cb4d41350bdb25f17ebfc12613
NedkoD <nedkoedno@mail.bg>
2020-07-01 20:58:53 +0300

    Add project files.

---
+++ TestTask1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTask1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTask1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dcbe120a-5802-446c-bd78-4fc744c582c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestTask1/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace TelerikLoginTestTask
+{
+    [TestFixture]
+    public class TelerikLoginTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.telerik.com/login/v2/telerik");
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void LoginTelerik()
+        {
+            //accept the user agreement
+            driver.FindElement(By.Id("onetrust-accept-btn-handler")).Click();
+
+            //send username and password
+            driver.FindElement(By.Id("username")).SendKeys("nedkoedno@mail.bg");
+            driver.FindElement(By.Id("password")).SendKeys("tralala");
+
+            var loginButton = wait.Until((w) => { return w.FindElement(By.Id("GeneralContent_C048_ctl00_ctl00_LoginButton")); });
+            loginButton.Click();
+
+            //check if the  page is loaded
+            var usernameField = wait.Until((w) => { return w.FindElement(By.XPath("//h3[@class='e2e-nickname u-wbba']")); });
+
+            Assert.AreEqual("Nedko", usernameField.Text);
+
+
+        }
+
+        [TearDown]
+        public void CloseDriver()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
